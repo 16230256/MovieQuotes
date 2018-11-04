71a0d4b9c4b36642b7819277450833263195bdab
16230256 <39533070+16230256@users.noreply.github.com>
2018-11-04 14:36:57 +0000

    changes per video

---
+++ app/src/main/java/ie/ul/deirdreshanahan/moviequotes/MainActivity.java
@@ -0,0 +1,96 @@
+package ie.ul.deirdreshanahan.moviequotes;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MQ";
+    private int mTempCounter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // temp firebase testing areas.
+        //to do comment out
+       final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("moviequotes")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Added some Firestore Data", Snackbar.LENGTH_LONG)
+                        .show();
+                // Create a new user with a first and last name
+                Map<String, Object> mq = new HashMap<>();
+                mTempCounter = mTempCounter + 1;
+                mq.put("quote", "Quote #" + mTempCounter);
+                mq.put("movie", "Movie #" + mTempCounter);
+                mq.put("born", 1815);
+
+// Add a new document with a generated ID
+                db.collection("moviequotes").add(mq);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
