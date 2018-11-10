97dc6d1356055d743f74b3ce5ccef2d47d327435
16230256 <39533070+16230256@users.noreply.github.com>
2018-11-10 21:59:08 +0000

    review Recycle Views

--- app/src/main/java/ie/ul/deirdreshanahan/moviequotes/MainActivity.java
+++ app/src/main/java/ie/ul/deirdreshanahan/moviequotes/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        MovieQuoteAdapter movieQuoteAdapter = new MovieQuoteAdapter();
+        recyclerView.setAdapter(movieQuoteAdapter);
 
         // temp firebase testing areas.
         //to do comment out
