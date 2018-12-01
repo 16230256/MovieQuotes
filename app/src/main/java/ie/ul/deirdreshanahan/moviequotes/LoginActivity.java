ee446960144f16b360c0c28fa47cad289e88aad2
16230256 <39533070+16230256@users.noreply.github.com>
2018-12-01 13:49:05 +0000

    new screen with user name -sign in
    Toast not  working

--- app/src/main/java/ie/ul/deirdreshanahan/moviequotes/LoginActivity.java
+++ app/src/main/java/ie/ul/deirdreshanahan/moviequotes/LoginActivity.java
@@ -1,7 +1,10 @@
 package ie.ul.deirdreshanahan.moviequotes;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void handlesSignIn(View view) {
+        Toast.makeText(this, "Sing in", Toast.LENGTH_LONG).show();
+    }
+    public void hanlesSingUp(View view){
+        Toast.makeText(this, "Sing Up", Toast.LENGTH_LONG).show();
     }
 }
